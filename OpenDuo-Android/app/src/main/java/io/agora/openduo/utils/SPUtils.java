87be482c5ce1538158ef6afe1ecbba5dfae246e0
brianxcli <neteasy1210@gmail.com>
2020-04-30 16:50:43 +0800

    Support id numbers that are less than 1000

--- OpenDuo-Android/app/src/main/java/io/agora/openduo/utils/SPUtils.java
+++ OpenDuo-Android/app/src/main/java/io/agora/openduo/utils/SPUtils.java
@@ -6,15 +6,13 @@ import android.content.SharedPreferences;
 import io.agora.openduo.Constants;
 
 public class SPUtils {
-    public static final int NO_USER = -1;
-
-    public static void saveUserId(Context context, int id) {
+    public static void saveUserId(Context context, String id) {
         SharedPreferences pf = context.getSharedPreferences(Constants.SP_NAME, Context.MODE_PRIVATE);
-        pf.edit().putInt(Constants.PREF_USER_ID, id).apply();
+        pf.edit().putString(Constants.PREF_USER_ID, id).apply();
     }
 
-    public static int getUserId(Context context) {
+    public static String getUserId(Context context) {
         SharedPreferences pf = context.getSharedPreferences(Constants.SP_NAME, Context.MODE_PRIVATE);
-        return pf.getInt(Constants.PREF_USER_ID, NO_USER);
+        return pf.getString(Constants.PREF_USER_ID, null);
     }
 }
